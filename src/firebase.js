ec34753833faadb697822f384d768a3bf9686ce0
saiful <saiful21july@gmail.com>
2021-02-26 20:22:58 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDggo7CFHJXHWzW6tgQn8cSMOyvj_Jy140",
+    authDomain: "clone-8a484.firebaseapp.com",
+    projectId: "clone-8a484",
+    storageBucket: "clone-8a484.appspot.com",
+    messagingSenderId: "702079685513",
+    appId: "1:702079685513:web:cfa2be8c8695504a8b0758",
+    measurementId: "G-ZV5B0Y2WPR"
+  };
